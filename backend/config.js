204ded57a0db44c11e00013f69291c7f974892f7
supritkumar shah <suprit7@gmail.com>
2017-10-28 13:53:00 -0400

    set up completed

---
+++ backend/config.js
@@ -0,0 +1,12 @@
+export const facebook = {
+  clientID: '146323949319268',
+  clientSecret: '4a765646a2e282663b904cedd0467f06',
+  callbackURL: 'http://localhost:3000/auth/facebook/callback',
+  profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
+};
+
+export const google = {
+  clientID: '172916012554-rg25khhbqockgs4m7f50312ijoe00h9q.apps.googleusercontent.com',
+  clientSecret: 'SzLJgLZsIXwhUprOzslFM_nO',
+  callbackURL: 'http://localhost:3000/auth/google/callback',
+};
